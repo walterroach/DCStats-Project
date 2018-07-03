60ebdd8198ebd48dacc858acfebbe3d75a36d6d6
Roach <wrhaynie@gmail.com>
2018-07-03 03:03:24 +0000

    can't git fer shit

--- lua/src/config.lua
+++ lua/src/config.lua
@@ -1,5 +1,3 @@
 paths = {
-  ["json"] = "C:\\Users\\Walter\\lua\\DCStats\\DCStats\\src\\JSON.lua",
-  ["stats"] = "D:\\DCS\\Willshouse\\stats\\SlmodStats.lua",
-  ["savedir"] = "C:\\Users\\Walter\\Desktop\\DCStats Django\\DCStats-Project\\slmod\\"
+  ["misstats"] = "/home/walter/DCStats/DCStats-Project/slmis"
 }
