a6094dc89fa549e6ef708939836d43827343dd3a
walterroach <37820425+walterroach@users.noreply.github.com>
2018-07-27 00:01:59 -0500

    Refactoring/Pylint

--- stats/query.py
+++ stats/query.py
@@ -1,49 +1,34 @@
-#dqueries
-from django.utils import timezone
-from .models import *
-from django.shortcuts import render
-from django.http import HttpResponse
+'''
+stats.query
+'''
+import datetime
 from django.db.models import Sum
-from django.db.models.functions import TruncDay
-
-def filter_date(datefilter):
-    if datefilter == 'all':
-        date_filter = all_dates()
-
-    elif datefilter == 'week':
-        date_filter = last_week()
-
-    elif datefilter == 'month':
-        date_filter = last_month()
-
-    elif datefilter == 'quarter':
-        date_filter = last_quarter()
-
-    return date_filter
+from .models import Stats
 
 def last_week(date):
+    '''Take datetime object and return datetime object 7 days prior'''
     start_date = date.replace(hour=0, minute=0, second=0, microsecond=0)
     start_date = start_date + datetime.timedelta(days=-7)
     return start_date
 
 def end_day(date):
+    '''Take datetime object and return datetime object with time set to 23 59 59 0'''
     end_date = date.replace(hour=23, minute=59, second=59, microsecond=0)
     return end_date
 
-# def all_dates():
-#   date_filter = datetime.datetime.now()
-#   date_filter = date_filter.replace(year=date_filter.year-50)
-#   return date_filter
-
-# def last_month():
-#   date_filter = datetime.datetime.now() + datetime.timedelta(-30)
-#   return date_filter
-
-# def last_quarter():
-#   date_filter = datetime.datetime.now() + datetime.timedelta(-90)
-#   return date_filter
-
 def execute(options):
+    '''
+    return dict of :models:'stats.Stats' queryset
+
+    **arg**
+    {
+     'group_by':groups,
+     'pilot_filter':clientid,
+     'aircraft_filter':aircraft_id
+     'start_date':datetime
+     'end_date':datetime
+     }
+    '''
     groups = options['group_by']
     if 'pilot__callsign' in groups:
         groups.append('pilot__user__userprofile__rank_id__rank')
@@ -51,69 +36,44 @@ def execute(options):
         groups.append('pilot__user__last_name')
     if 'mission__name' in groups:
         groups.append('mission__date')
-    missions = Stats.objects.filter(mission__date__range=(options['start_date'], options['end_date']))
+    missions = Stats.objects.filter(mission__date__range=(options['start_date'],
+                                                          options['end_date']))
     if options['pilot_filter']:
         missions = missions.filter(pilot=options['pilot_filter'])
     if options['aircraft_filter']:
         missions = missions.filter(aircraft=options['aircraft_filter'])
-        # pilot = Pilot.objects.get(clientid=clientid)
-        # name = str(pilot)
-        # rank = pilot.rank_id
     stats = missions.values(*groups) \
     .annotate(in_air_hours=Sum('in_air_sec') / 3600,
-        hours_on_server=Sum('total_sec') / 3600,
-        losses=Sum('losses'), ground_kills=Sum('ground_kills'),
-        aircraft_kills=Sum('aircraft_kills'), ship_kills=Sum('ship_kills'),
-        landings=Sum('landings'), traps=Sum('traps'), aar=Sum('aar')).order_by('-hours_on_server')
-
-    # if 'pilot' in groups:
-    #     for s in stats:
-    #         pobject = Pilot.objects.get(clientid=s['pilot'])
-    #         s['name'] = str(pobject)
-    #         # first_name = pobject.user.first_name
-    #         # last_name = pobject.user.last_name
-    #         # callsign = pobject.callsign
-    #         # s['name'] = first_name + ' "' + callsign + '" ' + last_name
-    #         try:
-    #             rank = pobject.user.userprofile.rank_id
-    #         except AttributeError:
-    #             rank = 'Guest'
-    #         s['rank'] = rank
-
-    print(stats)
+              hours_on_server=Sum('total_sec') / 3600,
+              losses=Sum('losses'),
+              ground_kills=Sum('ground_kills'),
+              aircraft_kills=Sum('aircraft_kills'),
+              ship_kills=Sum('ship_kills'),
+              landings=Sum('landings'),
+              traps=Sum('traps'),
+              aar=Sum('aar')).order_by('-hours_on_server')
     return stats
 
 def new_stats(user, options):
-    print(f"NEW STATS {options['start_date']}\n{options['end_date']}")
-    stats = Stats.objects.filter(pilot=user,
-                                 mission__date__range=(options['start_date'],
-                                 options['end_date'])).annotate(
-                                 in_air_hours=Sum('in_air_sec') / 3600,
-                                 hours_on_server=Sum('total_sec') /3600).order_by('-mission__date') \
-                                 .prefetch_related('pilot__user', 'pilot', 'mission', 'aircraft')
-    return stats 
+    '''
+    Return queryset of :models:'stats.Stats', and related
+    :models:'stats.Pilot' :models:'stats.aircraft' objects.
 
-# def execute(request, clientid, datefilter, **groups):
-#   groups = groups
-#   all_pilots = Pilot.objects.all()
-#   missions = Stats.objects.filter(date__gte=filter_date(datefilter))
-#   if clientid != 'all':
-#       missions = missions.filter(pilot=clientid)
-#       # pilot = Pilot.objects.get(clientid=clientid)
-#       # name = str(pilot)
-#       # rank = pilot.rank_id
-#   stats = missions.values(*groups.values()) \
-#           .annotate(in_air_hours=Sum('in_air_sec') / 3600,
-#           hours_on_server=Sum('total_sec') / 3600,
-#           losses=Sum('crash'),
-#           all_aircraft_kills=Sum('all_aircraft_kills'),
-#           surface_kills=Sum('building_kills') + Sum('ground_kills') + Sum('ship_kills')) \
-#           .order_by('-in_air_hours')
-#   if 'pilot' in groups.values():
-#       for s in stats:
-#           pobject = Pilot.objects.get(clientid=s['pilot'])
-#           name = str(pobject)
-#           rank = pobject.rank_id
-#           s['pilot'] = name
-#           s['rank'] = rank
-#   return render(request, 'stats/pilot_stats.html', {'stats':stats, 'pilots':all_pilots})
+    **args**
+    'user' as :models:'stats.Pilot' instance
+
+    'options' as dict
+        {'start_date':datetime, 'end_date':datetime}
+
+    '''
+    print(f"NEW STATS {options['start_date']}\n{options['end_date']}")
+    stats = Stats.objects.filter(
+        pilot=user,
+        mission__date__range=(options['start_date'],
+                              options['end_date'])) \
+                         .annotate(
+                             in_air_hours=Sum('in_air_sec') / 3600,
+                             hours_on_server=Sum('total_sec') /3600) \
+                         .order_by('-mission__date') \
+                         .prefetch_related('pilot__user', 'pilot', 'mission', 'aircraft')
+    return stats
