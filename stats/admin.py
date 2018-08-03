c1a194da2dc5c1fddf8867344e04314da6cd5603
walterroach <37820425+walterroach@users.noreply.github.com>
2018-08-03 10:25:51 -0500

    Added in process filter mission admin

--- stats/admin.py
+++ stats/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Pilot, Rank, Aircraft, Mission, Stats, UserProfile
 
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+	list_filter = ('in_process', 'date')
+
 admin.site.register(Pilot)
 admin.site.register(Rank)
 admin.site.register(Aircraft)
 admin.site.register(Stats)
-admin.site.register(Mission)
+# admin.site.register(Mission)
 admin.site.register(UserProfile)
