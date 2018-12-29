802ac304c7e74ab20efeec32a2f668a0beca5ac1
walterroach <37820425+walterroach@users.noreply.github.com>
2018-12-29 13:01:38 -0600

    Updated Google Auth

--- DCStats/settings.py
+++ DCStats/settings.py
@@ -116,7 +116,7 @@ AUTHENTICATION_BACKENDS = (
  'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
  'social_core.backends.google.GoogleOpenId',  # for Google authentication
  'social_core.backends.google.GoogleOAuth2',  # for Google authentication
- 
+ 'social_core.backends.google.GoogleOpenId',
  'django.contrib.auth.backends.ModelBackend',
 )
 
