60c63f5c40a7ac7ca6112c3df86a8473ba2d2a92
Walter <wrhaynie@gmail.com>
2022-03-18 17:24:15 -0500

    update admin page title

--- stats/admin.py
+++ stats/admin.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.contrib import admin
 
 from .models import Aircraft, Mission, Pilot, Rank, Stats, UserProfile
@@ -6,8 +7,10 @@ from .models import Aircraft, Mission, Pilot, Rank, Stats, UserProfile
 @admin.register(Mission)
 class MissionAdmin(admin.ModelAdmin):
     list_filter = ("in_process", "date")
-
-
+title_str = "21st VAWC Admin"
+admin.site.site_header = title_str
+admin.site.site_title = title_str
+admin.site.index_title = "Welcome to the " + title_str
 admin.site.register(Pilot)
 admin.site.register(Rank)
 admin.site.register(Aircraft)
